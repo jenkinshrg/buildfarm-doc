1f2c2a77630c5c578c51c23467bdd90ce5c053d6
jenkinshrg <jenkinshrg@gmail.com>
2016-02-05 16:58:54 +0900

    modify script

--- conf.py
+++ conf.py
@@ -52,8 +52,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'buildfarm'
-copyright = u'2016, Akira Tokunaga'
-author = u'Akira Tokunaga'
+copyright = u'2016, AIST'
+author = u'AIST'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -228,7 +228,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'buildfarm.tex', u'buildfarm Documentation',
-     u'Akira Tokunaga', 'manual'),
+     u'AIST', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
